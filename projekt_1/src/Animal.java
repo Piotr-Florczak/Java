d90171314ff13aaeb5210f22d823ac44028a8c95
kertoip2005 <81766931+kertoip2005@users.noreply.github.com>
2023-05-13 13:00:03 +0200

    Initial commit

---
+++ projekt_1/src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal
+{
+    private String imie;
+    Animal (String imie)
+    {
+        this.imie = imie;
+    }
+
+    public String getImie()
+    {
+            return imie;
+    }
+}
